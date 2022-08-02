cd58b014ab93f3655360063f955b3fdaa23aaa12
Blake Bourque <Techplex.Engineer@gmail.com>
2022-08-01 21:23:11 -0400

    Bucket name is a subdomain

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ if [ -f /pb_data/data.db ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-	litestream restore -v -if-replica-exists -o /pb_data/data.db
+	litestream restore -v -if-replica-exists -o /pb_data/data.db ${REPLICA_URL}
 fi
 
 # Run litestream with your app as the subprocess.
